c78f412a7a8c456f8bf0011f6d67abb68c336008
Jesse Bloom <jbloom@fredhutch.org>
2021-12-20 11:44:21 -0800

    update batch calculator

--- bindingcalculator.py
+++ bindingcalculator.py
@@ -25,11 +25,13 @@ class BindingCalculator:
     ----------
     csv_or_url : str
         Path to CSV or URL of CSV containing the escape data. Should
-        have columns 'condition', 'normalized', 'metric', and 'escape'.
+        have columns 'condition', 'metric', and 'escape'.
     eliciting_virus : {'all', 'SARS-CoV-2', 'SARS-CoV-1'}
         Include antibodies elicited by these viruses.
-    normalized : {True, False}
-        Whether to use normalized or non-normalized data.
+    source_lab : str
+        Include antiboies from this lab, use 'all' for all labs.
+    neutralizes_Omicron : {'yes', 'no', 'either'}
+        Just use antibodies that neutralize Omicron?
     metric : {'sum of mutations at site', 'mean of mutations at site'}
         Which escape metric to use.
     mutation_escape_strength : float
@@ -53,18 +55,18 @@ class BindingCalculator:
     to use the equivalent file on the main branch without specifying commit
     (this is URL that is the default value of `csv_or_url`):
 
-    >>> bindcalc = BindingCalculator(csv_or_url='https://raw.githubusercontent.com/jbloomlab/SARS2_RBD_Ab_escape_maps/9ac0417cf414423239511eceeb24abf86b58a067/processed_data/escape_calculator_data.csv')
+    >>> bindcalc = BindingCalculator(csv_or_url='https://raw.githubusercontent.com/jbloomlab/SARS2_RBD_Ab_escape_maps/651fe6fa5a7fccec2b662ddbb45b6d2c7421ae74/processed_data/escape_calculator_data.csv')
 
     We can look at the escape data after geting the specified normalization
     and metric and then scaling each escape value relative to max for condition:
 
     >>> bindcalc.escape_data.head()
-       condition       virus  site  normalized                    metric   escape  max_escape  scale_escape
-    0  COV2-2196  SARS-CoV-2   331        True  sum of mutations at site  0.01715         1.0       0.01715
-    1  COV2-2196  SARS-CoV-2   332        True  sum of mutations at site  0.02091         1.0       0.02091
-    2  COV2-2196  SARS-CoV-2   333        True  sum of mutations at site  0.01978         1.0       0.01978
-    3  COV2-2196  SARS-CoV-2   334        True  sum of mutations at site  0.02050         1.0       0.02050
-    4  COV2-2196  SARS-CoV-2   335        True  sum of mutations at site  0.02015         1.0       0.02015
+      condition       virus Omicron     lab  site                    metric  escape  max_escape  scale_escape
+    0    BD-739  SARS-CoV-2   False  Xie_XS   331  sum of mutations at site     0.0         1.0           0.0
+    1    BD-739  SARS-CoV-2   False  Xie_XS   332  sum of mutations at site     0.0         1.0           0.0
+    2    BD-739  SARS-CoV-2   False  Xie_XS   333  sum of mutations at site     0.0         1.0           0.0
+    3    BD-739  SARS-CoV-2   False  Xie_XS   334  sum of mutations at site     0.0         1.0           0.0
+    4    BD-739  SARS-CoV-2   False  Xie_XS   335  sum of mutations at site     0.0         1.0           0.0
 
     We can also check what sites have escape data. Here we just
     show min and max sites with data:
@@ -84,12 +86,12 @@ class BindingCalculator:
     With mutation at site 484:
 
     >>> round(bindcalc.binding_retained([484]), 3)
-    0.589
+    0.703
 
     With mutation at site 417 and 484:
 
     >>> round(bindcalc.binding_retained([417, 484]), 3)
-    0.457
+    0.514
 
     If you have a data frame of variants, you can just map the
     calculation of the binding retained to a new column, like this:
@@ -104,23 +106,25 @@ class BindingCalculator:
     >>> variants.round(3)
           variant mutated RBD sites  binding_retained
     0  Wuhan-Hu-1                []             1.000
-    1     B.1.351   [417, 484, 501]             0.419
-    2     B.1.1.7             [501]             0.930
-    3     B.1.429             [452]             0.877
+    1     B.1.351   [417, 484, 501]             0.505
+    2     B.1.1.7             [501]             0.980
+    3     B.1.429             [452]             0.930
 
     We can also calculate the escape remaining at each site after a mutation:
 
     >>> bindcalc.escape_per_site([417, 484]).query('site in [484, 486, 490]').round(3)
          site  original_escape  retained_escape
-    153   484            0.362            0.019
-    155   486            0.254            0.076
-    159   490            0.243            0.050
+    153   484            0.279            0.009
+    155   486            0.159            0.106
+    159   490            0.163            0.032
 
     """
     def __init__(self,
                  csv_or_url='https://raw.githubusercontent.com/jbloomlab/SARS2_RBD_Ab_escape_maps/main/processed_data/escape_calculator_data.csv',
+                 *,
                  eliciting_virus='SARS-CoV-2',
-                 normalized=True,
+                 source_lab='all',
+                 neutralizes_Omicron='either',
                  metric='sum of mutations at site',
                  mutation_escape_strength=2,
                  ):
@@ -131,9 +135,11 @@ class BindingCalculator:
         # make sure escape data has expected columns
         if not set(self.escape_data.columns).issuperset({'condition',
                                                          'site',
-                                                         'normalized',
                                                          'escape',
-                                                         'virus'}):
+                                                         'virus',
+                                                         'Omicron',
+                                                         'lab',
+                                                         }):
             raise ValueError(f"escape data in {csv_or_url} lacks expected columns")
 
         # filter by virus
@@ -142,14 +148,22 @@ class BindingCalculator:
                 raise ValueError(f"invalid {eliciting_virus=}")
             self.escape_data = self.escape_data.query('virus == @eliciting_virus')
 
-        # filter escape data to just the normalization and metric of interest
-        for col, filter_val in [('normalized', normalized), ('metric', metric)]:
-            if filter_val not in set(self.escape_data[col]):
-                raise ValueError(f"{filter_val} not a valid value for {col}; "
-                                 f"valid values are {set(self.escape_data[col])}")
-            self.escape_data = (self.escape_data[self.escape_data[col] == filter_val]
-                                .reset_index(drop=True)
-                                )
+        # filter by metric
+        if metric not in set(self.escape_data['metric']):
+            raise ValueError(f"invalid {metric=}")
+        self.escape_data = self.escape_data.query('metric == @metric')
+
+        # filter by source lab
+        if source_lab != 'all':
+            if source not in set(self.escape_data['lab']):
+                raise ValueError(f"invalid {source_lab=}")
+            self.escape_data = self.escape_data.query('lab == @source_lab')
+
+        # filter by neutralizes Omicron
+        if neutralizes_Omicron != 'either':
+            if neutralizes_Omicron not in set(self.escape_data['Omicron']):
+                raise ValueError(f"invalid {neutralizes_Omicron=}")
+            self.escape_data = self.escape_data.query('Omicron == @neutralizes_Omicron')
 
         # get escape scaled by the max escape for that condition
         self.escape_data = (
