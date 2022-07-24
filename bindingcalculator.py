ed4e540e3d2e9c4997e2ae25365943641d700d31
jbloom <jbloom@fredhutch.org>
2022-07-24 06:15:37 -0700

    add log IC50 weighting to binding calculator

--- bindingcalculator.py
+++ bindingcalculator.py
@@ -30,6 +30,8 @@ class BindingCalculator:
         Include antibodies elicited by these viruses.
     known_to_neutralize : str
         Include antibodies known to neutralize this virus.
+    weight_by_log_IC50 : bool
+        Weight antibodies by log IC50.
     mutation_escape_strength : float
         Scaling exponent :math:`s`; larger values mean stronger escape, see
         https://jbloomlab.github.io/SARS2_RBD_Ab_escape_maps/escape-calc/
@@ -42,6 +44,8 @@ class BindingCalculator:
     sites : set
         All sites for which we have escape data. We can only calculate effects
         of mutations at these sites.
+    weight_by_log_IC50 : bool
+        Value of `weight_by_log_IC50` passed as parameter.
 
     Example
     -------
@@ -55,12 +59,12 @@ class BindingCalculator:
     and metric and then scaling each escape value relative to max for condition:
 
     >>> bindcalc.escape_data.head()
-      condition  site   escape  max_escape  scale_escape
-    0      1-57   338  0.05792      0.9521      0.060834
-    1      1-57   359  0.01558      0.9521      0.016364
-    2      1-57   370  0.03169      0.9521      0.033284
-    3      1-57   394  0.01253      0.9521      0.013160
-    4      1-57   396  0.02160      0.9521      0.022687
+      condition  site   escape neg_log_IC50  max_escape  scale_escape
+    0      1-57   338  0.05792       7.3385      0.9521      0.060834
+    1      1-57   359  0.01558       7.3385      0.9521      0.016364
+    2      1-57   370  0.03169       7.3385      0.9521      0.033284
+    3      1-57   394  0.01253       7.3385      0.9521      0.013160
+    4      1-57   396  0.02160       7.3385      0.9521      0.022687
 
     We can also check what sites have escape data. Here we just
     show min and max sites with data:
@@ -80,12 +84,12 @@ class BindingCalculator:
     With mutation at site 484:
 
     >>> round(bindcalc.binding_retained([484]), 3)
-    0.836
+    0.817
 
     With mutation at site 417 and 484:
 
     >>> round(bindcalc.binding_retained([417, 484]), 3)
-    0.764
+    0.733
 
     If you have a data frame of variants, you can just map the
     calculation of the binding retained to a new column, like this:
@@ -100,13 +104,35 @@ class BindingCalculator:
     >>> variants.round(3)
           variant mutated RBD sites  binding_retained
     0  Wuhan-Hu-1                []             1.000
-    1     B.1.351   [417, 484, 501]             0.729
-    2     B.1.1.7             [501]             0.951
-    3     B.1.429             [452]             0.908
+    1     B.1.351   [417, 484, 501]             0.689
+    2     B.1.1.7             [501]             0.940
+    3     B.1.429             [452]             0.903
 
     We can also calculate the escape remaining at each site after a mutation:
 
-    >>> bindcalc.escape_per_site([417, 484]).query('site in [484, 486, 490]').round(3)
+    >>> bindcalc.escape_per_site([417, 484]).query('site in [484, 486, 490]')
+         site original_escape retained_escape
+    135   484        0.070931         0.00925
+    137   486        0.116009        0.103263
+    141   490        0.055852        0.022456
+
+    Now do the same but **not** weighting by log IC50:
+
+    >>> bindcalc_noweight = BindingCalculator(
+    ...     csv_or_url='processed_data/escape_calculator_data.csv',
+    ...     weight_by_log_IC50=False,
+    ... )
+
+    >>> bindcalc_noweight.binding_retained([])
+    1.0
+
+    >>> round(bindcalc_noweight.binding_retained([484]), 3)
+    0.836
+
+    >>> round(bindcalc_noweight.binding_retained([417, 484]), 3)
+    0.764
+
+    >>> bindcalc_noweight.escape_per_site([417, 484]).query('site in [484, 486, 490]').round(3)
          site  original_escape  retained_escape
     135   484            0.066            0.009
     137   486            0.080            0.067
@@ -114,24 +140,28 @@ class BindingCalculator:
 
     """
     def __init__(self,
-                 csv_or_url='https://raw.githubusercontent.com/jbloomlab/SARS2_RBD_Ab_escape_maps/main/processed_data/escape_calculator_data.csv',
-                 *,
-                 eliciting_virus='SARS-CoV-2',
-                 known_to_neutralize="any",
-                 mutation_escape_strength=2,
-                 ):
+        csv_or_url='https://raw.githubusercontent.com/jbloomlab/SARS2_RBD_Ab_escape_maps/main/processed_data/escape_calculator_data.csv',
+        *,
+        eliciting_virus='SARS-CoV-2',
+        known_to_neutralize="any",
+        weight_by_log_IC50=True,
+        mutation_escape_strength=2,
+    ):
         """See main class docstring."""
         # read escape data 
         self.escape_data = (
             pd.read_csv(csv_or_url)
-            .drop(columns="neg_log_IC50")
             .assign(
                 eliciting_virus=lambda x: x["eliciting_virus"].str.split(";"),
                 known_to_neutralize=lambda x: x["known_to_neutralize"].str.split(";"),
+                neg_log_IC50=lambda x: x["neg_log_IC50"].map(
+                    lambda s: tuple([pd.NA if si == "NA" else float(si) for si in s.split(";")])
+                )
             )
             .explode("eliciting_virus")
-            .explode("known_to_neutralize")
+            .explode(["known_to_neutralize", "neg_log_IC50"])
         )
+        assert all(self.escape_data["neg_log_IC50"] >= 0)
 
         # make sure escape data has expected columns
         if not set(self.escape_data.columns).issuperset({'condition',
@@ -139,6 +169,7 @@ class BindingCalculator:
                                                          'escape',
                                                          'eliciting_virus',
                                                          "known_to_neutralize",
+                                                         "neg_log_IC50",
                                                          }):
             raise ValueError(f"{self.escape_data.columns=} lacks expected columns")
 
@@ -178,8 +209,25 @@ class BindingCalculator:
         # set mutation escape strength
         self.mutation_escape_strength = mutation_escape_strength
 
-        # number of conditions (antibodies)
-        self._n_conditions = self.escape_data['condition'].nunique()
+        # do we weight by log IC50?
+        self.weight_by_log_IC50 = weight_by_log_IC50
+
+        assert (
+            self.escape_data["condition"].nunique()
+            == len(self.escape_data[["condition", "neg_log_IC50"]].drop_duplicates())
+        )
+
+        # number of conditions (antibodies), weighting by negative log IC50 if doing that
+        if self.weight_by_log_IC50:
+            self._n_conditions = (
+                self.escape_data
+                [["condition", "neg_log_IC50"]]
+                .drop_duplicates()
+                ["neg_log_IC50"]
+                .sum()
+            )
+        else:
+            self._n_conditions = self.escape_data['condition'].nunique()
 
     def escape_per_site(self, mutated_sites):
         """Escape at each site after mutating indicated sites.
@@ -205,20 +253,24 @@ class BindingCalculator:
                 mutated=lambda x: x['site'].isin(mutated_sites).astype(int),
                 site_bind_retain=lambda x: 1 - x['scale_escape'] * x['mutated']
                 )
-            .groupby('condition')
-            .aggregate(cond_bind_retain=pd.NamedAgg('site_bind_retain',
-                                                    'prod')
-                       )
-            ['cond_bind_retain']
-            .pow(self.mutation_escape_strength)
-            .reset_index()
+            .groupby(['condition', "neg_log_IC50"], as_index=False)
+            .aggregate(cond_bind_retain=pd.NamedAgg('site_bind_retain', "prod"))
+            .assign(
+                cond_bind_retain=lambda x: x["cond_bind_retain"].pow(self.mutation_escape_strength),
+                weight=lambda x: x["neg_log_IC50"] if self.weight_by_log_IC50 else 1,
+            )
+            [["condition", 'cond_bind_retain', "weight"]]
             .merge(self.escape_data[['condition', 'site', 'escape']])
-            .assign(retained_escape=lambda x: x['cond_bind_retain'] * x['escape'])
+            .assign(
+                escape=lambda x: x["escape"] * x["weight"],
+                retained_escape=lambda x: x['cond_bind_retain'] * x['escape'],
+            )
             .groupby('site')
-            .aggregate(original_escape=pd.NamedAgg('escape', 'sum'),
-                       retained_escape=pd.NamedAgg('retained_escape', 'sum'),
-                       )
-            ) / self._n_conditions
+            .aggregate(
+                original_escape=pd.NamedAgg('escape', 'sum'),
+                retained_escape=pd.NamedAgg('retained_escape', 'sum'),
+            )
+        ) / self._n_conditions
         return df.reset_index()
 
     def binding_retained(self, mutated_sites):
@@ -242,16 +294,18 @@ class BindingCalculator:
             self.escape_data
             .assign(
                 mutated=lambda x: x['site'].isin(mutated_sites).astype(int),
-                site_bind_retain=lambda x: 1 - x['scale_escape'] * x['mutated']
-                )
-            .groupby('condition')
-            .aggregate(cond_bind_retain=pd.NamedAgg('site_bind_retain',
-                                                    'prod')
-                       )
-            ['cond_bind_retain']
-            .pow(self.mutation_escape_strength)
+                site_bind_retain=lambda x: 1 - x['scale_escape'] * x['mutated'],
+            )
+            .groupby(['condition', 'neg_log_IC50'], as_index=False)
+            .aggregate(cond_bind_retain=pd.NamedAgg('site_bind_retain', "prod"))
+            .assign(
+                cond_bind_retain=lambda x: x["cond_bind_retain"].pow(self.mutation_escape_strength),
+                weight=lambda x: x["neg_log_IC50"] if self.weight_by_log_IC50 else 1,
+                weighted_cond_bind_retain=lambda x: x["cond_bind_retain"] * x["weight"],
+            )
+            ['weighted_cond_bind_retain']
             .sum()
-            ) / self._n_conditions
+        ) / self._n_conditions
         return binding_retained
 
 
